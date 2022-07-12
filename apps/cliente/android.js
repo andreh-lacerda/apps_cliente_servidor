49f911b31aea9049e576756b3ced38236ebcc1ff
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-07-12 18:43:39 -0300

    feat: criando middlewares

---
+++ apps/cliente/android.js
@@ -0,0 +1,32 @@
+var http = require("http");
+
+var opcoes = {
+  hostname: "localhost",
+  port: 80,
+  path: "/",
+  method: "post",
+  headers: {
+    Accept: "application/json",
+    Content-type: "application/json"
+  }
+}
+
+var html = "nome=André";
+var json = {nome : 'André'};
+var string_json = JSON.stringify(json);
+
+var buffer_corpo_response = [];
+
+var req = http.request(opcoes, function (res) {
+  res.on("data", function (pedaco) {
+    buffer_corpo_response.push(pedaco);
+  });
+
+  res.on("end", function () {
+    var corpo_response = Buffer.concat(buffer_corpo_response).toString();
+    console.log(corpo_response);
+  });
+});
+
+req.write(string_json);
+req.end();
